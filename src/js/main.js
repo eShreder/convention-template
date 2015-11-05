d7bc77e8e34e9db0a5d1a0e6c6b1fb9b810fab21
eShreder <eshreder@gmail.com>
2015-11-06 01:35:00 +0300

    sync

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 ;$(function(){
 	var FADEIN_SPEED = 400;
 	var preload_image = $('img[src*="preload"]');
+	var viewPort = $(window).height();
 	preload_image.each(function(i, obj){
 		var $obj = $(obj),
 			img = $('<img>').attr('src', obj.src.replace('/preload','')),
@@ -14,15 +15,30 @@
 		})
 	})
 
-	$('.bg.paralax').each(function(index, el){
-		var _e = [$(el).offset().top, $(el)];
-		$(window).on('scroll', function(e){
-			var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-			if(_e[0] <= scrolled){
-				_e[1].css({
-					'transform':'translate3d(0,' + - (_e[0] - scrolled)/2 + 'px, 0)'
+	var bP = $('.bg.paralax').map(function(index, el){
+		return [[$(el).offset().top, $(el)]];
+	});
+	var bF = $('.bg .fade.active').map(function(index, el){
+		return [[$(el).offset().top, $(el)]];
+	});
+	$(window).on('resize', function(e){
+		viewPort = $(window).height();
+	});
+	$(window).on('scroll', function(e){
+		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+		for(var i = 0; i < bP.length; i++) {
+			if(bP[i][0] <= scrolled){
+				bP[i][1].css({
+					'transform':'translate3d(0,' + - (bP[i][0] - scrolled)/2 + 'px, 0)'
 				});
 			}
-		})
+		}
+		for(var i = 0; i < bF.length; i++) {
+			if(bF[i][0] <= scrolled + viewPort){
+				bF[i][1].css({
+					'opacity': Math.min(0.83, 1 - (bF[i][0] - scrolled)/viewPort)
+				});
+			}
+		}
 	});
 });

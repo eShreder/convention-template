1648ddae9af4c80451995f6a6f3214d4f267dec1
eShreder <eshreder@gmail.com>
2015-11-11 08:06:58 +0300

    add mediaquerys

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,52 @@
+(function ($) {
+
+    $.fn.parallax = function () {
+      var window_width = $(window).width();
+      return this.each(function(i) {
+        var $this = $(this);
+        var this_height = $this.height();
+        var top = $this.offset().top;
+        var bottom = $this.offset().top + this_height;
+
+        function updateParallax(initial) { 
+          var scrollTop = $(window).scrollTop();
+          var windowHeight = window.innerHeight;
+          var windowBottom = scrollTop + windowHeight;
+          
+
+          if(top <= scrollTop){
+				$this.css({
+					'transform':'translate3d(0,' + - (top - scrollTop)/2 + 'px, 0)'
+				});
+			} else {
+				$this.css({
+					'transform':'translate3d(0,0,0)'
+				});
+			}
+        }
+
+        // Wait for image load
+        $this.children("img").one("load", function() {
+          updateParallax(true);
+        }).each(function() {
+          if(this.complete) $(this).load();
+        });
+
+        $(window).scroll(function() {
+          window_width = $(window).width();
+          updateParallax(false);
+        });
+
+        $(window).resize(function() {
+          window_width = $(window).width();
+          updateParallax(false);
+        });
+
+      });
+
+    };
+}( jQuery ));
+
 ;$(function(){
 	var FADEIN_SPEED = 400;
 	var preload_image = $('img[src*="preload"]');
@@ -14,7 +63,7 @@
 			});
 		})
 	})
-
+	$('.bg.paralax').parallax();
 	var bP = $('.bg.paralax').map(function(index, el){
 		return [[$(el).offset().top, $(el)]];
 	});
@@ -26,13 +75,17 @@
 	});
 	$(window).on('scroll', function(e){
 		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-		for(var i = 0; i < bP.length; i++) {
-			if(bP[i][0] <= scrolled){
-				bP[i][1].css({
-					'transform':'translate3d(0,' + - (bP[i][0] - scrolled)/2 + 'px, 0)'
-				});
-			}
-		}
+		// for(var i = 0; i < bP.length; i++) {
+		// 	if(bP[i][0] <= scrolled){
+		// 		bP[i][1].css({
+		// 			'transform':'translate3d(0,' + - (bP[i][0] - scrolled)/2 + 'px, 0)'
+		// 		});
+		// 	} else {
+		// 		bP[i][1].css({
+		// 			'transform':'translate3d(0,0,0)'
+		// 		});
+		// 	}
+		// }
 		for(var i = 0; i < bF.length; i++) {
 			if(bF[i][0] <= scrolled + viewPort){
 				bF[i][1].css({
@@ -41,4 +94,8 @@
 			}
 		}
 	});
+
+	function updateParalax(obj){
+
+	}
 });
